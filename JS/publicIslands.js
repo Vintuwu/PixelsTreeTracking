8d79ef54f1670c34d8fa73f3c3d73dded2debbec
Lautaro Gonzalez <lautarofglez@gmail.com>
2024-03-05 03:10:02 -0300

    Fix: Really fixed & clean code

--- JS/publicIslands.js
+++ JS/publicIslands.js
@@ -1,32 +1,8 @@
-function publicRandom(array){
-    var island
-    var publicSeeds = []
-    for (var i=0; i<4; i++){
-        island = Math.floor((Math.random() * array.length));
-        publicSeeds.push(array[island]);
-    }
-    return publicSeeds
-}
-
-function updateLands(someArray){
-    var seeds = publicRandom(someArray);
-    var aux = 0
-    for (var i=0; i<4; i++){
-        aux = i+1;
-        $('#iframe' + aux).attr('src', 'https://play.pixels.xyz/pixels/share/' + seeds[i])
-        $('#copySeed' + aux).on('click', function(){
-            navigator.clipboard.writeText(seeds[i])
-        })
-        $('#seed' + aux).val(seeds[i]);
-    }
-}
-
 $(document).ready(function(){
-    var publicIslands = [111,294,331,582,604,691,872,905,970,989,1361,1362,1422,1563,1615,1862,1947,1984,2025,2053,2265,2309,2330,2441,2584,2706,2727,2812,2841,2843,3195,3279,3318,3378,3481,3572,3676,3862,3884,3917,4268,4366,4534,4697,4752,4783,4801,4924,4962,4980,1313,1979,1716,3376,4846,4330,4085,1354,1357,2280,973]
     //?? 3467 4060
-    updateLands(publicIslands);
+    updateLands(publicRandom());
 
     $('#random').on('click',function(){
-        updateLands(publicIslands);
+        updateLands(publicRandom());
     })
 })
--- JS/search.js
+++ JS/search.js
@@ -1,55 +1,7 @@
-function updateLands(){
-    var collection = randomizer();
-    var aux = 0
-
-    for (var i=0; i<4; i++){
-        aux = i+1;
-        $('#iframe' + aux).attr('src', 'https://play.pixels.xyz/pixels/share/' + collection[i])
-        $('#copySeed' + aux).on('click', function(){
-            navigator.clipboard.writeText(collection[i])
-        })
-        $('#seed' + aux).val(collection[i]);
-    }
-}
-
 $(document).ready(function(){
-    updateLands()
-    $('#search').on('click', function(){
-        var newSeed = $('#seed').val();
-
-        if (newSeed === 0){
-            newSeed = Math.floor((Math.random() * 5000) +1);
-
-            $('#iframe1').attr('src', 'https://play.pixels.xyz/pixels/share/' + newSeed);
-            $('#copySeed1').on('click', function() {
-                navigator.clipboard.writeText(newSeed)
-            });
-            $('#iframe2').attr('src', 'https://play.pixels.xyz/pixels/share/' + (parseInt(newSeed) + 1));
-            $('#copySeed2').on('click', function() {
-                navigator.clipboard.writeText(parseInt(newSeed) + 1)
-            });
-            $('#iframe3').attr('src', 'https://play.pixels.xyz/pixels/share/' + (parseInt(newSeed) + 2));
-            $('#copySeed3').on('click', function() {
-                navigator.clipboard.writeText(parseInt(newSeed) + 2)
-            });
-            $('#iframe4').attr('src', 'https://play.pixels.xyz/pixels/share/' + (parseInt(newSeed) + 3));
-            $('#copySeed4').on('click', function() {
-                navigator.clipboard.writeText(parseInt(newSeed) + 3)
-            });
-
-        } else {    
-            
-            $('#iframe1').attr('src', 'https://play.pixels.xyz/pixels/share/' + newSeed);
-            $('#iframe2').attr('src', 'https://play.pixels.xyz/pixels/share/' + (parseInt(newSeed) + 1));
-            $('#iframe3').attr('src', 'https://play.pixels.xyz/pixels/share/' + (parseInt(newSeed) + 2));
-            $('#iframe4').attr('src', 'https://play.pixels.xyz/pixels/share/' + (parseInt(newSeed) + 3));
-        }
-
-        return false;
-    });
-
+    updateLands(randomize())
     $('#random').on('click', function(){
-        updateLands()
+        updateLands(randomize())
         return false;
     });
 });
